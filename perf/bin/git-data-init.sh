9126a3442c05d10d8d2b0b7d4c99c2940724a380
35V LG84 <35vlg84-x4e6b92@e257.fi>
2025-04-26 15:47:08 +0300

    test: make 'git-data-init.sh' stable
    * Change 'git-data-init.sh' so that generated repository
      is stable (e.g. will have same commit ids on repeatable runs)
    * Make commit at the end of each day
    * Generate tags for each month
    
    [skip ci]
    
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- perf/bin/git-data-init.sh
+++ perf/bin/git-data-init.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # vim: tabstop=4 shiftwidth=4 smarttab expandtab softtabstop=4 autoindent
 #
-# Tackler-NG 2018-2024
-#
+# Tackler-NG 2018-2025
 # SPDX-License-Identifier: Apache-2.0
 #############################################################################
 
-
+# Initialize test repository for tackler git backend
+#
+# Test data is generated with pta-generator
+# (https://github.com/tackler-ng/pta-generator/)
+#
+# This will produce stable git repository (e.g. commit ids) on repeatable runs.
+#
+# For historic reasons this data set is generated for year 2016:
+#    pta-generator audit --path sets --shard-type txn \
+#      --start 2016-01-01T00:00:00+0000[UTC] \
+#      --stop  2017-01-01T00:00:00+0000[UTC] \
+#      --set-size ...
+#
 usage () {
     echo "Initialize and populate git repository with test data"
     echo
-	echo "Usage: $0 <repo name> <1E1 | 1E2 | 1E3 | 1E4 | 1E5 | 1E6>"
+	echo "Usage: $0 <repo name> <1e1 | 1e2 | 1e3 | 1e4 | 1e5 | 1e6>"
 }
 
 if [ $# != 2 ]; then
@@ -18,17 +29,18 @@ if [ $# != 2 ]; then
     exit 1
 fi
 
-repo_name=$1
+repo_name="$1"
 
-name=txns-$2
-store="../$name"
+set="$2"
+name="set-$set"
+store="$(pwd)/sets/audit/${name}-txn"
 
-if [ ! -d $name ]; then
+if [ ! -d "$store" ]; then
 	echo "Error: $name not found"
 	exit 1
 fi
 
-if [ ! -d $repo_name ]; then
+if [ ! -d "$repo_name" ]; then
     git init --bare "$repo_name.git"
     git clone "$repo_name.git"
     
@@ -38,65 +50,101 @@ if [ ! -d $repo_name ]; then
     git config user.email "accounting@example.com"
     git config commit.gpgSign no
     git config gc.autoDetach no
-    
-    git commit --allow-empty -m "init"
+
+    ts="2015-12-31T10:00:00+0000"
+    GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+        git commit --allow-empty -m "init"
     cd ..
 fi
 
-cd $repo_name
+cd "$repo_name"
 
 if [ ! -e readme.txt ]; then
     echo "Tackler test repository for git storage backend" > readme.txt
     echo >> readme.txt
-    echo "See different branches for available sets" >> readme.txt
+    echo "Structure of the repository" >> readme.txt
+    echo " * Each test set is own branch" >> readme.txt
+    echo " * Transaction directory is 'txns'" >> readme.txt
+    echo " * Txn shard is by year, month and day (YYYY/MM/DD)" >> readme.txt
+    echo " * Each txn is separate file, named by timestamp and index" >> readme.txt
     echo >> readme.txt
+    echo "Available git objects" >> readme.txt
+    echo " * branch for each test set" >> readme.txt
+    echo " * tags by month for each set" >> readme.txt
+    echo " * commit time is the time of the last txn of the day" >> readme.txt
+    echo >> readme.txt
+    echo "Available test branches:" >> readme.txt
 
     git add readme.txt
-    git commit -m 'Initial readme for main'
+    ts="2015-12-31T12:00:00+0000"
+    GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+        git commit -m 'init (main)'
     git push --set-upstream origin main
 fi
 
 git checkout main
 
 
+set_min=$(echo $set | tr -d 'e')
+
 echo " * $name" >> readme.txt
 git add readme.txt
-git commit -m "$name" readme.txt
+ts="2015-12-31T14:${set_min}:00+0000"
+GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+    git commit -m "readme: $name" readme.txt
 git push
 
-git checkout -b $name
+git checkout -b "$name"
 
 echo "set: $name" > "info.txt"
 git add "info.txt"
-git commit -m "$name: initial"
+ts="2016-01-01T00:00:00+0000"
+GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+    git commit -m "$name: init"
  
 mkdir -p txns
 mkdir -p txns/2016
 
-for m in 01 02 03 04 05 06 07 08 09 10 11 12; do
+for M in $(seq 1 12); do
+    m=$(printf "%02d" $M)
 
-    src="$store/2016/$m"
+    src="$store/txns/2016/$m"
+    trg="txns/2016/$m"
 
-    echo "Perf: start $name, round: $m"
+    echo "step: $name, month: $m"
 
     # All sets does't have all months
     if [ ! -d "$src" ]; then
-        echo "Perf: skip  $name, round: $m"
+        echo "step: skip  $name, month: $m"
         echo
         continue
     fi
-    cp -a "$src" txns/2016/
-    git add txns
-    git commit -m "$name: 2016/$m"
-    echo
-    echo "Do git gc next ..."
+    cp -a "$src" "$trg"
+
+    for D in $(seq 1 31); do
+        d=$(printf '%02d' $D)
+        trg_d="$trg/$d"
+
+        if [ ! -d "$trg_d" ]; then
+            echo "step: skip $name, month: $m, day: $d"
+            continue
+        fi
+        git add "$trg_d"
+
+        ts=$(ls $trg_d/*.txn | sort |tail -n1 | sed 's@.*/2016\(.*\)-[0-9]\+.txn@2016\1Z@')
+        GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+            git commit -m "$name: 2016-$m-$d"
+    done
+    GIT_AUTHOR_DATE="$ts" GIT_COMMITTER_DATE="$ts" \
+        git tag -a -m "$name: 2016-$m" "s${set}_2026-$m"
+    echo "step: git gc"
     git gc
     
     echo 
-    echo "Perf: done $name, round: $m"
-    # make sure that git timestamps are distinct
-    sleep 3
+    echo "done $name, round: $m"
 done
 
-git push --set-upstream origin $name
+echo "step: git gc --prune=now"
+git gc --prune=now
 
+git push --set-upstream origin $name
