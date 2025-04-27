ee588c81dcb7290668ceb0de0e36f3c6d8e34bd1
35V LG84 <35vlg84-x4e6b92@e257.fi>
2025-04-27 21:05:41 +0300

    test: add fd250432-9b13-4cdd-83a1-1aedff1593ed
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- tests/sh/cli.sh
+++ tests/sh/cli.sh
@@ -29,3 +29,41 @@ $TACKLER_SH \
 echo -n "check:"
 cmp_result $module $test_name txn identity
 echo ": ok"
+
+#
+# test: fd250432-9b13-4cdd-83a1-1aedff1593ed
+# desc: UTF-8 output
+rm -f $OUTPUT_DIR/*
+test_name=console-03
+echo "test: $module/$test_name: $mode"
+
+$TACKLER_SH \
+    --config $SUITE_PATH/basic-json.toml \
+    --output.dir $OUTPUT_DIR \
+    --output.prefix $test_name \
+    --input.file $SUITE_PATH/$module/ok/console-txns/id-chars.txn \
+    --reports balance balance-group register \
+    --exports identity equity
+
+$TACKLER_SH \
+    --config $SUITE_PATH/basic.toml \
+    --input.file $SUITE_PATH/$module/ok/console-txns/id-chars.txn \
+    --reports balance balance-group register \
+     > "${OUTPUT_DIR}/${test_name}.stdout.txt" \
+     2> "${OUTPUT_DIR}/${test_name}.stderr.txt"
+
+echo -n "check:"
+cmp_result $module $test_name txt bal
+cmp_result $module $test_name txt balgrp
+cmp_result $module $test_name txt reg
+
+cmp_result $module $test_name txn identity
+cmp_result $module $test_name txn equity
+
+cmp_result $module $test_name json bal
+cmp_result $module $test_name json balgrp
+cmp_result $module $test_name json reg
+
+cmp_result $module $test_name txt stdout
+cmp_result $module $test_name txt stderr
+echo ": ok"
